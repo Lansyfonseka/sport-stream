22e9cb73897eed1bca8aa5a6423017b4dba40726
GoodFeeller <maksim.vasilenok@gmail.com>
2025-10-15 23:06:38 +0300

    Feat: change port

--- src/config/app.config.js
+++ src/config/app.config.js
@@ -1,12 +1,12 @@
-const baseFrontUrlDev = 'http://localhost:5173';
-const baseBackUrlDev = 'http://localhost:3001';
+const baseFrontUrlDev = 'http://localhost:5173'
+const baseBackUrlDev = 'http://localhost:3005'
 
-const baseFrontUrlProd = 'https://your-frontend-domain.com';
-const baseBackUrlProd = 'https://your-backend-domain.com';
+const baseFrontUrlProd = 'https://your-frontend-domain.com'
+const baseBackUrlProd = 'https://your-backend-domain.com'
 
-const USE_PRODUCTION = false;
+const USE_PRODUCTION = false
 
 export const AppConfig = {
   baseFrontUrl: USE_PRODUCTION ? baseFrontUrlProd : baseFrontUrlDev,
   baseBackUrl: USE_PRODUCTION ? baseBackUrlProd : baseBackUrlDev,
-};
+}
