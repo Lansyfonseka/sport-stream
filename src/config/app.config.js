b14cde8c75e78ce8a39151e292c5f20aceb8b239
GoodFeeller <maksim.vasilenok@gmail.com>
2025-10-21 00:43:06 +0300

    Feat: change ip

--- src/config/app.config.js
+++ src/config/app.config.js
@@ -1,13 +1,13 @@
 const baseFrontUrlDev = 'http://localhost:5173'
 const baseBackUrlDev = 'http://localhost:3005'
 
-const baseFrontUrlProd = 'http://193.227.240.252:5173'
-const baseBackUrlProd = 'http://193.227.240.252:3005'
+const baseFrontUrlProd = 'http://212.80.205.187'
+const baseBackUrlProd = 'http://212.80.205.187:3005'
 
-const USE_PRODUCTION = true;
+const USE_PRODUCTION = true
 
 // получать расписание с фронтенда (true) или с бэкенда (false)
-const USE_FRONTEND_SCHEDULE = true;
+const USE_FRONTEND_SCHEDULE = true
 
 export const AppConfig = {
   baseFrontUrl: USE_PRODUCTION ? baseFrontUrlProd : baseFrontUrlDev,
