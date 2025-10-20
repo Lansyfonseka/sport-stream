c96a835ddeca9c7f81de49c7f916c9ca4b4734f3
DzianisBychuck <dzianis.bychuk.dev@gmail.com>
2025-10-20 09:02:58 +0300

    fix

--- src/components/EmbeddedStream/EmbeddedStream.jsx
+++ src/components/EmbeddedStream/EmbeddedStream.jsx
@@ -1,20 +1,43 @@
-import { useEffect, useRef, useState } from "react"
-import "./_embedded-stream.scss"
+import { useEffect, useRef, useState } from "react";
+import "./_embedded-stream.scss";
 
 export default function EmbeddedStream({ src, showLoader }) {
-  const iframeRef = useRef(null)
-  const [iframeSrc, setIframeSrc] = useState(src)
-  const containerRef = useState(null)
+  const iframeRef = useRef(null);
+  const [iframeSrc, setIframeSrc] = useState(src);
+  const containerRef = useState(null);
+  const [isResizing, setIsResizing] = useState(false);
+
   useEffect(() => {
     const timeout = setTimeout(() => {
-      setIframeSrc(`${src}?t=${new Date().getTime()}`)
-    }, 5000)
-    return () => clearTimeout(timeout)
-  }, [src])
+      setIframeSrc(`${src}?t=${new Date().getTime()}`);
+    }, 5000);
+    return () => clearTimeout(timeout);
+  }, [src]);
 
+  useEffect(() => {
+    let resizeTimeout;
+
+    const handleResize = () => {
+      if (iframeRef.current) {
+        setIsResizing(true);
+        setIframeSrc(`${src}?t=${new Date().getTime()}`);
+
+        clearTimeout(resizeTimeout);
+        resizeTimeout = setTimeout(() => {
+          setIsResizing(false);
+        }, 3000);
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+      clearTimeout(resizeTimeout);
+    };
+  }, [src]);
 
   return (
-    <div ref={containerRef} className='embedded-stream' >
+    <div ref={containerRef} className="embedded-stream">
       <iframe
         ref={iframeRef}
         src={iframeSrc}
@@ -25,19 +48,17 @@ export default function EmbeddedStream({ src, showLoader }) {
         allow="autoplay; fullscreen"
       />
       <div className="embedded-stream__blur-corner">
-        <img src='/princebet77_logo.svg' />
+        <img src="/princebet77_logo.svg" />
       </div>
       <div className="embedded-stream__blur-corner-fullscreen" />
 
       <div className="embedded-stream__clickBlocker" />
-      {
-        (showLoader) && (
-          <div className="embedded-stream__loader">
-            <div className="embedded-stream__spinner" />
-            <p className="embedded-stream__loader-text">...טוען ערוץ</p>
-          </div>
-        )
-      }
-    </div >
-  )
+      {(showLoader || isResizing) && (
+        <div className="embedded-stream__loader">
+          <div className="embedded-stream__spinner" />
+          <p className="embedded-stream__loader-text">...טוען ערוץ</p>
+        </div>
+      )}
+    </div>
+  );
 }
