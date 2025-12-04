32f5268ccd25a68828392851cc3d40a366d58675
DzianisBychuck <dzianis.bychuk.dev@gmail.com>
2025-12-04 12:46:30 +0300

    fix: changed banner gif

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,15 +1,23 @@
-import { useState } from 'react'
-import BannerGif from '../../assets/side-banner.gif'
-import './_banner.scss'
+import { useState } from "react";
+import BannerGif from "../../assets/newBaner.gif";
+import "./_banner.scss";
 
-export default function Banner({ side = 'left' }) {
-  const [display, setDisplay] = useState('block')
+export default function Banner({ side = "left" }) {
+  const [display, setDisplay] = useState("block");
 
   return (
-    <div className={'c-banner' + (side ? ' ' + side : '')} style={{ display: display }}>
-      <a href="https://heylink.me/PrinceBet77" className='c-banner__link' target="_blank" rel="nofollow noopener">
+    <div
+      className={"c-banner" + (side ? " " + side : "")}
+      style={{ display: display }}
+    >
+      <a
+        href="https://heylink.me/PrinceBet77"
+        className="c-banner__link"
+        target="_blank"
+        rel="nofollow noopener"
+      >
         <img src={BannerGif} alt="banner" />
       </a>
     </div>
-  )
+  );
 }
